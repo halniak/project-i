ca143cc65d452911a39bb7b0b465563c320380ee
Maciej Michalik <halniak24@gmail.com>
2021-09-04 14:03:49 +0200

    Update responsive page layout

--- src/js/script.js
+++ src/js/script.js
@@ -2,27 +2,37 @@ let sidebarTrigger = {};
 
 sidebarTrigger.logo = document.querySelector('.sidebar__toggler');
 sidebarTrigger.checkbox = document.getElementById('check');
+sidebarTrigger.checkbox.checked = true;
 
-const menu = document.querySelector('.menu__wrapper');
-let sideBar = document.querySelector('.sidebar');
+const menuContainer = document.querySelector('#menu');
+const pageBody = document.querySelector('.main');
+let sidebarContainer = document.querySelector('.sidebar');
+let headerContainer = document.querySelector('#header');
+let btnQuit = document.querySelector('#quit-label');
+let managerContainer = document.querySelector('#manager');
 
-sidebarTrigger.logo.addEventListener('click', sidebarToggle);
-sidebarTrigger.checkbox.addEventListener('click', sidebarToggle);
+adjustMenu();
 
-function sidebarToggle(event) {
+sidebarTrigger.logo.addEventListener('click', function sidebarToggle(event) {
   event.preventDefault();
   const sidebar = document.querySelector('.sidebar');
-  const mainPage = document.querySelector('.main');
 
   sidebarTrigger.checkbox.checked ? sidebarTrigger.checkbox.checked = false : sidebarTrigger.checkbox.checked = true;
-
   sidebar.classList.toggle('active');
-  mainPage.classList.toggle('active');
-}
-
-window.addEventListener('resize', function () {
-  if (window.innerWidth < 768) {
-    sideBar.appendChild(menu);
-  }
+  pageBody.classList.toggle('active');
 });
 
+window.addEventListener('resize', adjustMenu);
+
+function adjustMenu() {
+  if (window.innerWidth < 575) {
+    sidebarContainer.appendChild(menuContainer);
+    btnQuit.innerHTML = 'Logout';
+    managerContainer.classList.remove('active');
+  }
+  else {
+    headerContainer.appendChild(menuContainer);
+    btnQuit.innerHTML = '';
+    managerContainer.classList.add('active');
+  }
+}
