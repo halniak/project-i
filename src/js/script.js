72a9a21d6f8a08d66d5ce7c50febefc855fbc3e0
Maciej Michalik <halniak24@gmail.com>
2021-09-06 16:35:47 +0200

    Add styleguide part 1

--- src/js/script.js
+++ src/js/script.js
@@ -1,38 +1,80 @@
-let sidebarTrigger = {};
+/* Settings */
+const select = {
+  menu: '#menu',
+  pageBody: '.main',
+  sidebar: {
+    container: '.sidebar',
+    logo: '.sidebar__toggler',
+  },
+  header: '#header',
+  btnQuit: '#quit-label',
+  manager: '#manager',
+  wallet: '#wallet',
+  notification: {
+    element: '#notification',
+    badge: '#notification__toggler > .notification__badge',
+  },
+  menuToggleSections: '.menu__elem.expanded',
+};
 
-sidebarTrigger.logo = document.querySelector('.sidebar__toggler');
-sidebarTrigger.checkbox = document.getElementById('check');
-sidebarTrigger.checkbox.checked = true;
+const elements = {
+  menuContainer: document.querySelector(select.menu),
+  pageBody: document.querySelector(select.pageBody),
+  sidebarContainer: document.querySelector(select.sidebar.container),
+  headerContainer: document.querySelector(select.header),
+  btnQuit: document.querySelector(select.btnQuit),
+  managerContainer: document.querySelector(select.manager),
+  walletContainer: document.querySelector(select.wallet),
+  notificationContainer: document.querySelector(select.notification.element),
+  notificationBadge: document.querySelector(select.notification.badge),
+  sidebarTrigger: {
+    logo: document.querySelector(select.sidebar.logo),
+    checkbox: document.getElementById('check'),
+  }
+};
 
-const menuContainer = document.querySelector('#menu');
-const pageBody = document.querySelector('.main');
-let sidebarContainer = document.querySelector('.sidebar');
-let headerContainer = document.querySelector('#header');
-let btnQuit = document.querySelector('#quit-label');
-let managerContainer = document.querySelector('#manager');
-
+/* Init actions */
+elements.sidebarTrigger.checkbox.checked = true;
 adjustMenu();
+addListeners();
 
-sidebarTrigger.logo.addEventListener('click', function sidebarToggle(event) {
-  event.preventDefault();
-  const sidebar = document.querySelector('.sidebar');
-
-  sidebarTrigger.checkbox.checked ? sidebarTrigger.checkbox.checked = false : sidebarTrigger.checkbox.checked = true;
-  sidebar.classList.toggle('active');
-  pageBody.classList.toggle('active');
-});
-
-window.addEventListener('resize', adjustMenu);
-
+/* Functions */
 function adjustMenu() {
   if (window.innerWidth < 575) {
-    sidebarContainer.appendChild(menuContainer);
-    btnQuit.innerHTML = 'Logout';
-    managerContainer.classList.remove('active');
+    elements.sidebarContainer.appendChild(elements.menuContainer);
+    elements.btnQuit.innerHTML = 'Logout';
+    elements.managerContainer.classList.remove('active');
   }
   else {
-    headerContainer.appendChild(menuContainer);
-    btnQuit.innerHTML = '';
-    managerContainer.classList.add('active');
+    elements.headerContainer.appendChild(elements.menuContainer);
+    elements.btnQuit.innerHTML = '';
+    elements.managerContainer.classList.add('active');
+  }
+}
+
+function addListeners() {
+  elements.sidebarTrigger.logo.addEventListener('click', function sidebarToggle(event) {
+    event.preventDefault();
+    const sidebar = document.querySelector('.sidebar');
+
+    elements.sidebarTrigger.checkbox.checked ? elements.sidebarTrigger.checkbox.checked = false : elements.sidebarTrigger.checkbox.checked = true;
+    sidebar.classList.toggle('active');
+    elements.pageBody.classList.toggle('active');
+  });
+  elements.walletContainer.addEventListener('click', toggleMenuSection);
+  elements.notificationContainer.addEventListener('click', toggleMenuSection);
+  window.addEventListener('resize', adjustMenu);
+}
+
+function toggleMenuSection() {
+  const activeMenuSection = document.querySelector(select.menuToggleSections);
+  this.classList.toggle('expanded');
+
+  if (this !== activeMenuSection && activeMenuSection !== null) {
+    activeMenuSection.classList.remove('expanded');
+  }
+
+  if (this.id == 'notification') {
+    elements.notificationBadge.classList.toggle('active');
   }
 }
